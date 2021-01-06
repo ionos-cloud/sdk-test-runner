37c0d8afbee0631af1f52b7b6f7df91b0e7a0aca
Florin Mihalache <florin.mihalache@ionos.com>
2021-01-06 17:57:07 +0200

    feat: toLower filter

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,9 +1,11 @@
 import {ArrayFilter} from './array.filter'
 import {NumberFilter} from './number.filter'
 import {BoolFilter} from './bool.filter'
+import {ToLowerFilter} from './tolower.filter'
 
 import filterService from '../services/filter.service'
 
 filterService.register(new NumberFilter())
 filterService.register(new ArrayFilter())
 filterService.register(new BoolFilter())
+filterService.register(new ToLowerFilter())
